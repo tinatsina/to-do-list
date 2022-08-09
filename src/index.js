664f47e3119b4d5f6da852450098b891de04949f
tinayetsina <tinayetsina@outlook.com>
2022-08-09 15:27:11 +0800

    core ToDo setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import './style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const clicked = document.querySelector('.clicked');
+
+  clicked.addEventListener('click', () => {
+    alert('You clicked me!');
+  });
+});
+
 console.log('Hello Webpack');

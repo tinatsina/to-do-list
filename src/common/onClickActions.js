f1f9db108cc6ae6b6e0848d14de674b9462fd716
tinayetsina <tinayetsina@outlook.com>
2022-08-11 13:35:52 +0800

    update - removed onClick.js from project

--- src/common/onClickActions.js
+++
@@ -1,9 +0,0 @@
-// import ToDoList from './ToDoList.js';
-
-// const onclickList = new ToDoList();
-
-export default function onClickEvents() {
-  document.querySelector('.add-to-list-icon').addEventListener('click', () => {
-    // alert('Add item');
-  });
-}
